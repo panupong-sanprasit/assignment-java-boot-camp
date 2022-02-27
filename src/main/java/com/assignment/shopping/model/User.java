8b5d3a5a48fcf0ee4d8af0fde3a43f6366d85f5b
Tum-Panupong <panupong.sanprasit@aia.com>
2022-02-27 18:06:09 +0700

    #10 Create User Model and Repository

---
+++ src/main/java/com/assignment/shopping/model/User.java
@@ -0,0 +1,61 @@
+package com.assignment.shopping.model;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    @Column(name="user_name")
+    private String userName;
+
+    @Column(name="passwd")
+    private String password;
+
+    @Column(name="enabled")
+    private boolean enabled;
+
+    @Column(name="first_name")
+    private String firstName;
+
+    @Column(name="last_name")
+    private String lastName;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public boolean isEnabled() {
+        return enabled;
+    }
+
+    public void setEnabled(boolean enabled) {
+        this.enabled = enabled;
+    }
+
+}
---
+++ src/main/java/com/assignment/shopping/repository/UserRepository.java
@@ -0,0 +1,14 @@
+package com.assignment.shopping.repository;
+
+import com.assignment.shopping.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findById(Long id);
+
+}
